736158cf4e50c051145eeab871a331fb3ecb5112
Takayuki Sato <sttk.xslet@gmail.com>
2026-03-16 20:18:21 +0900

    test: added integration tests for sentinel and cluster configurations (#69)

---
+++ tests/sentinel_async_test.rs
@@ -0,0 +1,78 @@
+#[cfg(feature = "sentinel-async")]
+#[cfg(test)]
+mod integration_tests_of_sentinel_async {
+    use override_macro::{overridable, override_with};
+    use redis::AsyncCommands;
+    use sabi::tokio::{logic, setup_async, uses, DataAcc, DataHub};
+    use sabi_redis::{RedisSentinelAsyncDataConn, RedisSentinelAsyncDataSrc};
+
+    #[overridable]
+    trait MyDataAsync {
+        async fn get_greeting_async(&mut self) -> errs::Result<String>;
+        async fn say_greeting_async(&mut self, greeting: &str) -> errs::Result<()>;
+    }
+
+    #[overridable]
+    trait GettingAsyncDataAcc: DataAcc {
+        async fn get_greeting_async(&mut self) -> errs::Result<String> {
+            Ok("Hello".to_string())
+        }
+    }
+
+    #[overridable]
+    trait RedisAsyncSayingDataAcc: DataAcc {
+        async fn say_greeting_async(&mut self, greeting: &str) -> errs::Result<()> {
+            let data_conn = self
+                .get_data_conn_async::<RedisSentinelAsyncDataConn>("redis_sentinel")
+                .await?;
+            let mut redis_conn = data_conn.get_connection_async().await?;
+
+            redis_conn
+                .set::<_, _, ()>("greeting", greeting)
+                .await
+                .map_err(|e| errs::Err::with_source("fail to set greeting", e))?;
+
+            data_conn
+                .add_force_back_async(async |mut redis_conn| {
+                    redis_conn
+                        .del::<_, ()>("greeting")
+                        .await
+                        .map_err(|e| errs::Err::with_source("fail to force back", e))?;
+                    Ok(())
+                })
+                .await;
+
+            Ok(())
+        }
+    }
+
+    impl GettingAsyncDataAcc for DataHub {}
+    impl RedisAsyncSayingDataAcc for DataHub {}
+
+    #[override_with(GettingAsyncDataAcc, RedisAsyncSayingDataAcc)]
+    impl MyDataAsync for DataHub {}
+
+    async fn my_logic_async(data: &mut impl MyDataAsync) -> errs::Result<()> {
+        let greeting = data.get_greeting_async().await?;
+        data.say_greeting_async(&greeting).await
+    }
+
+    async fn my_app_async() -> errs::Result<()> {
+        let mut data = DataHub::new();
+        data.txn_async(logic!(my_logic_async)).await
+    }
+
+    #[tokio::test]
+    async fn test() -> errs::Result<()> {
+        uses("redis_sentinel", RedisSentinelAsyncDataSrc::new(
+          vec![
+            "redis://127.0.0.1:26479",
+            "redis://127.0.0.1:26480",
+            "redis://127.0.0.1:26481",
+          ],
+          "mymaster",
+        ))?;
+        let _auto_shutdown = setup_async().await?;
+        my_app_async().await
+    }
+}
---
+++ tests/sentinel_sync_test.rs
@@ -0,0 +1,77 @@
+#[cfg(feature = "sentinel-sync")]
+#[cfg(test)]
+mod integration_tests_of_sentinel_sync {
+    use override_macro::{overridable, override_with};
+    use redis::TypedCommands;
+    use sabi::{uses, DataAcc, DataHub};
+    use sabi_redis::{RedisSentinelDataConn, RedisSentinelDataSrc};
+
+    #[overridable]
+    trait MyData {
+        fn get_greeting(&mut self) -> errs::Result<String>;
+        fn say_greeting(&mut self, greeting: &str) -> errs::Result<()>;
+    }
+
+    #[overridable]
+    trait GettingDataAcc: DataAcc {
+        fn get_greeting(&mut self) -> errs::Result<String> {
+            Ok("Hello!".to_string())
+        }
+    }
+
+    #[overridable]
+    trait RedisSayingDataAcc: DataAcc {
+        fn say_greeting(&mut self, greeting: &str) -> errs::Result<()> {
+            let data_conn = self.get_data_conn::<RedisSentinelDataConn>("redis_sentinel")?;
+            let mut redis_conn = data_conn.get_connection()?;
+
+            if let Err(e) = redis_conn.set("greeting", greeting) {
+                return Err(errs::Err::with_source("fail to set greeting", e));
+            }
+
+            data_conn.add_force_back(|redis_conn| {
+                if let Err(e) = redis_conn.del("greeting") {
+                    return Err(errs::Err::with_source("fail to force back", e));
+                }
+                Ok(())
+            });
+
+            Ok(())
+        }
+    }
+
+    impl GettingDataAcc for DataHub {}
+    impl RedisSayingDataAcc for DataHub {}
+
+    #[override_with(GettingDataAcc, RedisSayingDataAcc)]
+    impl MyData for DataHub {}
+
+    fn my_logic(data: &mut impl MyData) -> errs::Result<()> {
+        let greeting = data.get_greeting()?;
+        data.say_greeting(&greeting)
+    }
+
+    fn my_app() -> errs::Result<()> {
+        let mut data = DataHub::new();
+        data.txn(my_logic)
+    }
+
+    #[test]
+    fn test() -> errs::Result<()> {
+        uses(
+          "redis_sentinel",
+          RedisSentinelDataSrc::new(
+            vec![
+              "redis://127.0.0.1:26479",
+              "redis://127.0.0.1:26480",
+              "redis://127.0.0.1:26481",
+            ],
+            "mymaster",
+          ),
+        )?;
+
+        let _auto_shutdown = sabi::setup()?;
+
+        my_app()
+    }
+}
